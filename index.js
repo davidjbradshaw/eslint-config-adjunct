f57c40f1a074912cd1325fab36b9de2d8b46f5bf
David Bradshaw <dave@Davids-MacBook-Air.local>
2019-11-19 14:12:38 +0000

    Add code to test which packages are currently installed and prompt user to install missing packages

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
+const isModuleAvailable = require('./lib/isModuleAvailable')
+
+// eslint-disable-next-line unicorn/prevent-abbreviations
+const moduleNotAvailable = pkg => !isModuleAvailable(pkg)
+
+const rules = [
+  'array-func',
+  'eslint-comments',
+  'html',
+  'json-format',
+  'markdown',
+  'no-constructor-bind',
+  'no-secrets',
+  'no-use-extend-native',
+  'optimize-regex',
+  'promise',
+  'sonarjs',
+  'simple-import-sort',
+  'switch-case',
+  'unicorn'
+]
+
+const notInstalled = rules.map(plugin => `eslint-plugin-${plugin}`).filter(moduleNotAvailable)
+
+if (notInstalled.length !== 0) {
+  console.log(`
+Oops! Something went wrong! :(
+
+EsLint-config-adjunct could not find the following package(s)
+
+  ${notInstalled.join('\n  ')}
+
+To install the missing packages, please run the following command:
+
+npm install ${notInstalled.join(' ')} --save-dev
+
+  `)
+  process.exit(1) // eslint-disable-line unicorn/no-process-exit
+}
+
 module.exports = {
-  extends: [
-    './rules/array-func',
-    './rules/eslint-comments',
-    './rules/html',
-    './rules/json-format',
-    './rules/markdown',
-    './rules/no-constructor-bind',
-    './rules/no-secrets',
-    './rules/no-use-extend-native',
-    './rules/optimize-regex',
-    './rules/promise',
-    './rules/sonar',
-    './rules/simple-import-sort',
-    './rules/switch-case',
-    './rules/unicorn',
-  ].map(require.resolve),
+  extends: rules.map(plugin => require.resolve(`./rules/${plugin}`)),
 }
