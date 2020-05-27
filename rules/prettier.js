2634b10f847bd8557262dadaa7ef0407a51b83d0
David Bradshaw <dave.bradshaw@smartpension.co.ukl>
2020-05-27 19:47:12 +0100

    Autoconfigure prettier configs for other eslint plugins

--- rules/prettier.js
+++ rules/prettier.js
@@ -1,5 +1,28 @@
+const { hasAnyDep } = require('../lib/utils')
+
+const configs = ['prettier']
+
+const optionalConfigs = [
+  ['@typescript-eslint/eslint-plugin', 'prettier/@typescript-eslint'],
+  'babel',
+  'flowtype',
+  'react',
+  'standard',
+  'vue',
+  'unicorn',
+]
+
+optionalConfigs.forEach((optConfig) => {
+  const config =
+    typeof optConfig === 'string'
+      ? [`eslint-plugin-${optConfig}`, `prettier/${optConfig}`]
+      : optConfig
+
+  if (hasAnyDep(config[0])) configs.push(config[1])
+})
+
 module.exports = {
-  extends: ['prettier', 'prettier/react', 'prettier/unicorn'],
+  extends: configs,
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
