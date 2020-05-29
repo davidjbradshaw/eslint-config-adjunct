3838cddbbb0e8b0bd6c62742e6f23d9546838c7b
David J. Bradshaw <dave@bradshaw.net>
2020-05-29 14:35:58 +0100

    Add missing rules to Cypress

--- rules/cypress.js
+++ rules/cypress.js
@@ -4,4 +4,10 @@ module.exports = {
   },
   extends: ['plugin:cypress/recommended'],
   plugins: ['cypress'],
+  rules: {
+    // Rules not in recommmend config
+    'cypress/assertion-before-screenshot': 0,
+    'cypress/no-force': 0,
+    'cypress/require-data-selectors': 0,
+  },
 }
