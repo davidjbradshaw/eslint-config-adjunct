534fee15b5a322ea95b486adb10afa2aaae937fd
David Bradshaw <dave.bradshaw@smartpension.co.ukl>
2020-05-30 20:48:53 +0100

    Add disabled rules to mocha cleanup

--- rules/mocha-cleanup.js
+++ rules/mocha-cleanup.js
@@ -1,4 +1,10 @@
 module.exports = {
   extends: ['plugin:mocha-cleanup/recommended-no-limits'],
   plugins: ['mocha-cleanup'],
+  rules: {
+    'mocha-cleanup/asserts-limit': 0,
+    'mocha-cleanup/complexity-it': 0,
+    'mocha-cleanup/disallow-stub-window': 0,
+    'mocha-cleanup/disallowed-usage': 0,
+  },
 }
