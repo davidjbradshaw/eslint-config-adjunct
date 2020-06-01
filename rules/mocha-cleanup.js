a21554c4a3d4412527441656e3cc1aeb664f22d5
David Bradshaw <dave.bradshaw@smartpension.co.ukl>
2020-06-01 14:40:15 +0100

    Add comment

--- rules/mocha-cleanup.js
+++ rules/mocha-cleanup.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['plugin:mocha-cleanup/recommended-no-limits'],
   plugins: ['mocha-cleanup'],
   rules: {
+    // Rules are not in recommended config
     'mocha-cleanup/asserts-limit': 0,
     'mocha-cleanup/complexity-it': 0,
     'mocha-cleanup/disallow-stub-window': 0,
