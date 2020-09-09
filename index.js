8b6575f991c2e11bb9983cd70124b62ac4050db3
poteat <me@mpote.at>
2020-09-09 15:41:26 -0700

    Comment out debugging logs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const checkMissing = require('./lib/missing')
-const showLoaded = require('./lib/loaded')
+// const showLoaded = require('./lib/loaded')
 const { rules, extraInstallPackage } = require('./configs')
 
 // Workaround VS Code trying to run this file twice!
 if (!global.hasAdjunctLoaded) {
   checkMissing(rules, extraInstallPackage)
-  showLoaded(rules, [])
+  // showLoaded(rules, [])
 
   // Disable some rules in unit tests
   rules.push('test-overrides')
