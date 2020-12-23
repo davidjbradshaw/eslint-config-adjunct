729b598f95fed088f1765425b36ca6d7095af7c8
David J. Bradshaw <dave@bradshaw.net>
2020-12-23 16:28:08 +0000

    Update simple-import-sort with new rules

--- rules/simple-import-sort.js
+++ rules/simple-import-sort.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: ['simple-import-sort'],
   rules: {
-    'simple-import-sort/sort': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     'sort-imports': 'off',
     'import/order': 'off',
   },
